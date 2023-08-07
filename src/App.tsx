985ac6e64821837df252108edb3535a7b99b4a8b
useharm <cfgkk.evsey@gmail.com>
2023-08-07 22:45:57 +0300

    Code splitting Lazy Suspence

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { Counter } from './components/Counter';
+import { Route, Routes } from 'react-router-dom';
 import './index.scss';
+import { Link } from 'react-router-dom';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { Suspense } from 'react';
 
 function App() {
   return (
     <div className='app'>
-        Hello world
-        <Counter />
+        <Link to='/'>Main page</Link>
+        <Link to='/about'>About page</Link>
+        <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+            <Route path='/' element={<MainPageAsync />}></Route>
+            <Route path='/about' element={<AboutPageAsync />}></Route>
+        </Routes>
+        </Suspense>
     </div>
   )
 }
---
+++ src/pages/MainPage/MainPage.async.tsx
@@ -0,0 +1,5 @@
+import { lazy } from 'react';
+
+export const MainPageAsync = lazy(() => new Promise(resolve => setTimeout(() => resolve(
+    // @ts-ignore
+    import('./MainPage')), 1500)));
