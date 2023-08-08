9675f0ba3619677b86f095353d7d78997c73fd05
useharm <cfgkk.evsey@gmail.com>
2023-08-08 23:47:34 +0300

    added scss variables, custom hook for themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
-import './index.scss';
+import './styles/index.scss';
 import { Link } from 'react-router-dom';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
-import { Suspense } from 'react';
+import { Suspense, useContext, useState } from 'react';
+import { Theme, ThemeContext } from './theme/ThemeContext';
+import { useTheme } from './theme/useTheme';
 
 function App() {
+  const { theme, changeTheme} = useTheme();
+
   return (
-    <div className='app'>
+    <div className={`app ${theme}`}>
+        <button onClick={() => changeTheme()}>toggle theme</button>
         <Link to='/'>Main page</Link>
         <Link to='/about'>About page</Link>
         <Suspense fallback={<div>Loading...</div>}>
