74910a33113fcea62e693b3c2493db9096a978ff
useharm <cfgkk.evsey@gmail.com>
2023-11-13 22:42:31 +0300

    added jest with first test case

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,21 @@
+import {classNames} from './classNames';
+
+describe('classNames', () => {
+	test('with only first param', () => {
+		expect(classNames('someClass')).toBe('someClass');
+	});
+
+	test('with additional class', () => {
+		expect(classNames('someClass', {}, ['class1', 'class2'])).toBe('someClass class1 class2');
+	});
+
+	test('with mods', () => {
+		const expected = 'someClass class1 class2 hovered scrollable';
+		expect(classNames('someClass', {hovered: true, scrollable: true}, ['class1', 'class2'])).toBe(expected);
+	});
+
+	test('with false mods', () => {
+		const expected = 'someClass class1 class2 scrollable';
+		expect(classNames('someClass', {hovered: false, scrollable: true}, ['class1', 'class2'])).toBe(expected);
+	});
+});
